9f6e05479f2cf3bfcf1a40f36449ea1fd4c28e1e
Jarles <jarlesjrlz@gmail.com>
2025-07-14 01:25:37 -0300

    deploy

--- components/aboutus.jsx
+++ components/aboutus.jsx
@@ -28,9 +28,9 @@ const AboutUs = () => {
         <div className="mt-8 text-2xl sm:text-4xl mx-auto text-white">
           {t("TextAboutUs5")}
         </div>
-        <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 gap-6 ">
+        <div className="mt-20 flex flex-wrap justify-center gap-x-6 gap-y-10">
           <div>
-            <div className="relative max-w-md mx-auto md:max-w-2xl mt-6 min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-xl mt-16 min-h-[500px]">
+            <div className="relative max-w-sm mx-auto md:max-w-sm mt-6 min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-xl mt-16 min-h-[500px]">
               <div className="px-6">
                 <div className="flex flex-wrap justify-center">
                   <div className="w-full flex justify-center">
@@ -84,63 +84,9 @@ const AboutUs = () => {
               </div>
             </div>
           </div>
-          <div>
-            <div className="relative max-w-md mx-auto md:max-w-2xl mt-6 min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-xl mt-16 min-h-[500px]">
-              <div className="px-6">
-                <div className="flex flex-wrap justify-center">
-                  <div className="w-full flex justify-center">
-                    <div className="relative">
-                      <img
-                        src="/images/69062592.png"
-                        className="shadow-xl rounded-full align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-[150px]"
-                      />
-                    </div>
-                  </div>
-                  <div className="w-full text-center mt-20">
-                    <div className="flex justify-center lg:pt-4 pt-6 pb-0">
-                      <div className=" text-center">
-                        <span className="text-xl font-bold block uppercase tracking-wide text-slate-500">
-                          Developer
-                        </span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="text-center">
-                  <h3 className="text-2xl text-slate-700 font-bold leading-normal mb-1">
-                    Ricardo Pla
-                  </h3>
-                  <div className="text-xs mt-0 mb-2 text-slate-400 font-bold uppercase">
-                    <i className="fas fa-map-marker-alt mr-2 text-slate-400 opacity-75"></i>
-                    Buenos Aires, Argentina
-                  </div>
-                </div>
-                <div className="mt-6 py-6 border-t border-slate-200 text-center">
-                  <div className="flex flex-wrap justify-center">
-                    <div className="w-full px-4">
-                      <p className="font-light leading-relaxed text-slate-600 mb-4 text-sm">
-                        {t("TextAboutUs7")}
-                      </p>
 
-                      <div className="flex justify-center">
-                        <a href="" className="mx-2">
-                          <BsInstagram color="gray" size={30} />
-                        </a>
-                        <a href="" className="mx-2">
-                          <BsLinkedin color="gray" size={30} />
-                        </a>
-                        <a href="" className="mx-2">
-                          <BsGithub color="gray" size={30} />
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
           <div>
-            <div className="relative max-w-md mx-auto md:max-w-2xl mt-6 min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-xl mt-16 min-h-[500px]">
+            <div className="relative max-w-sm mx-auto md:max-w-sm mt-6 min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-xl mt-16 min-h-[500px]">
               <div className="px-6">
                 <div className="flex flex-wrap justify-center">
                   <div className="w-full flex justify-center">
@@ -194,8 +140,9 @@ const AboutUs = () => {
               </div>
             </div>
           </div>
+
           <div>
-            <div className="relative max-w-md mx-auto md:max-w-2xl mt-6 min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-xl mt-16 min-h-[500px]">
+            <div className="relative max-w-sm mx-auto md:max-w-sm mt-6 min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-xl mt-16 min-h-[500px]">
               <div className="px-6">
                 <div className="flex flex-wrap justify-center">
                   <div className="w-full flex justify-center">
@@ -230,6 +177,117 @@ const AboutUs = () => {
                     <div className="w-full px-4">
                       <p className="font-light leading-relaxed text-slate-600 mb-4 text-sm">
                         {t("TextAboutUs9")}
+                      </p>
+
+                      <div className="flex justify-center">
+                        <a href="" className="mx-2">
+                          <BsInstagram color="gray" size={30} />
+                        </a>
+                        <a href="" className="mx-2">
+                          <BsLinkedin color="gray" size={30} />
+                        </a>
+                        <a href="" className="mx-2">
+                          <BsGithub color="gray" size={30} />
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="relative max-w-sm mx-auto md:max-w-sm mt-6 min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-xl mt-16 min-h-[500px]">
+              <div className="px-6">
+                <div className="flex flex-wrap justify-center">
+                  <div className="w-full flex justify-center">
+                    <div className="relative">
+                      <img
+                        src="/images/jarles.png"
+                        className="shadow-xl rounded-full align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-[150px]"
+                      />
+                    </div>
+                  </div>
+                  <div className="w-full text-center mt-20">
+                    <div className="flex justify-center lg:pt-4 pt-6 pb-0">
+                      <div className=" text-center">
+                        <span className="text-xl font-bold block uppercase tracking-wide text-slate-500">
+                          Developer
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="text-center">
+                  <h3 className="text-2xl text-slate-700 font-bold leading-normal mb-1">
+                    Jarles Sayhare
+                  </h3>
+                  <div className="text-xs mt-0 mb-2 text-slate-400 font-bold uppercase">
+                    <i className="fas fa-map-marker-alt mr-2 text-slate-400 opacity-75"></i>
+                    Ceará, Brazil
+                  </div>
+                </div>
+                <div className="mt-6 py-6 border-t border-slate-200 text-center">
+                  <div className="flex flex-wrap justify-center">
+                    <div className="w-full px-4">
+                      <p className="font-light leading-relaxed text-slate-600 mb-4 text-sm">
+                        {t("TextAboutUs7")}
+                      </p>
+
+                      <div className="flex justify-center">
+                        <a href="" className="mx-2">
+                          <BsInstagram color="gray" size={30} />
+                        </a>
+                        <a href="" className="mx-2">
+                          <BsLinkedin color="gray" size={30} />
+                        </a>
+                        <a href="" className="mx-2">
+                          <BsGithub color="gray" size={30} />
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <div className="relative max-w-sm mx-auto md:max-w-sm mt-6 min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-xl mt-16 min-h-[500px]">
+              <div className="px-6">
+                <div className="flex flex-wrap justify-center">
+                  <div className="w-full flex justify-center">
+                    <div className="relative">
+                      <img
+                        src="/images/jocean.png"
+                        className="shadow-xl rounded-full align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-[150px]"
+                      />
+                    </div>
+                  </div>
+                  <div className="w-full text-center mt-20">
+                    <div className="flex justify-center lg:pt-4 pt-6 pb-0">
+                      <div className=" text-center">
+                        <span className="text-xl font-bold block uppercase tracking-wide text-slate-500">
+                          Developer
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="text-center">
+                  <h3 className="text-2xl text-slate-700 font-bold leading-normal mb-1">
+                    Jocean Ferreira
+                  </h3>
+                  <div className="text-xs mt-0 mb-2 text-slate-400 font-bold uppercase">
+                    <i className="fas fa-map-marker-alt mr-2 text-slate-400 opacity-75"></i>
+                    João Pessoa, Brazil
+                  </div>
+                </div>
+                <div className="mt-6 py-6 border-t border-slate-200 text-center">
+                  <div className="flex flex-wrap justify-center">
+                    <div className="w-full px-4">
+                      <p className="font-light leading-relaxed text-slate-600 mb-4 text-sm">
+                        {t("TextAboutUs10")}
                       </p>
 
                       <div className="flex justify-center">
